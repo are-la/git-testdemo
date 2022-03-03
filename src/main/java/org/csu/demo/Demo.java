46b2e9531ad2239bde1bf56b7558be9f4d57e479
are-la <2801153431@qq.com>
2022-03-03 21:28:00 +0800

    initial git

---
+++ src/main/java/org/csu/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.csu.demo;
+
+import org.junit.Test;
+
+public class Demo {
+
+    @Test
+    public void x() {
+        System.out.println("test");
+    }
+
+}
---
+++ src/main/java/org/csu/demo/cm.java
@@ -0,0 +1,8 @@
+package org.csu.demo;
+
+public class cm {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.x();
+    }
+}
